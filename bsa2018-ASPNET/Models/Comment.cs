02a0e925ca5f6fb073ae06eca7041e8a153d2996
Ян <gorshkov.yan1998@gmail.com>
2018-07-07 22:06:33 +0300

    Was added models from previous project

---
+++ bsa2018-ASPNET/Models/Comment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace bsa2018_ASPNET.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public DateTime CreateAt { get; set; }
+        public string Body { get; set; }
+        public int UserId { get; set; }
+        public int PostId { get; set; }
+        public int Likes { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Body} - {CreateAt.ToString("d")}\nLikes - {Likes}";
+        }
+    }
+}
---
+++ bsa2018-ASPNET/Models/ToDo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace bsa2018_ASPNET.Models
+{
+    public class ToDo
+    {
+        public int Id { get; set; }
+        public DateTime CreateAt { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+        public int UserId { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} - {IsComplete}\n{CreateAt.ToString("d")}";
+        }
+    }
+}
