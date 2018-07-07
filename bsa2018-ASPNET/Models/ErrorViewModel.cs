41c9fae122fa82ccf4979d6a15e7566e8824d5f7
Ян <gorshkov.yan1998@gmail.com>
2018-07-07 21:33:17 +0300

    Was created the ASP.NET Core MVC project

---
+++ bsa2018-ASPNET/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace bsa2018_ASPNET.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
