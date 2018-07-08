fbc6c7352cd10dc1d69ab7067aeb4e5623c187d1
Ян <gorshkov.yan1998@gmail.com>
2018-07-08 11:32:17 +0300

    Was improved the fourth query view

--- bsa2018-ASPNET/wwwroot/js/site.js
+++ bsa2018-ASPNET/wwwroot/js/site.js
@@ -1,1 +1,13 @@
 ﻿// Write your JavaScript code.
+
+$(".showTable").click(function (event) {
+    var idTable = "todos" + $(this).attr("data-id");
+    if ($(this).text() == "Click for show") {
+        $(this).text("Click for hide");
+        $("#" + idTable).show();
+    }
+    else {
+        $(this).text("Click for show");
+        $("#" + idTable).hide();
+    }
+});
